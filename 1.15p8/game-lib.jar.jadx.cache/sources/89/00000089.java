0047a2eca8314c979bb6946033ed83ddc0e25401
Hcat <admin@hcat.work>
2022-07-19 18:51:17 +0800

    Remove caches

--- 1.15p8/game-lib.jar.jadx.cache/sources/89/00000089.java
+++
@@ -1,9 +0,0 @@
-package com.corrodinggames.rts.game.units;
-
-/* renamed from: com.corrodinggames.rts.game.units.aa */
-/* loaded from: game-lib.jar:com/corrodinggames/rts/game/units/aa.class */
-public enum EnumC0226aa {
-    attack,
-    move,
-    newSelection
-}
